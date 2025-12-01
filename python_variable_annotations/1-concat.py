78127d01713ab793872b3b6a5bad31a2354725c3
Victor <victor10days@gmail.com>
2025-11-30 23:36:16 -0400

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that takes two strings as arguments and returns concatenated string."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result.
+
+    Args:
+        str1: First string
+        str2: Second string
+    Returns:
+        The concatenated string
+    """
+    return str1 + str2
