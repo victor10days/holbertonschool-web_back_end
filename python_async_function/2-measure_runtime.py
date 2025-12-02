d1005277b3656033e0c3e3b6091048777d49398b
Victor <victor10days@gmail.com>
2025-12-02 10:23:28 -0400

    autopep

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,6 +6,7 @@ import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the total runtime of wait_n coroutine."""
     start_time = time.time()
