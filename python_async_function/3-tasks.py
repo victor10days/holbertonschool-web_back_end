ed4f177825239b1f05ff6839913c34ce1065bd3a
Victor <victor10days@gmail.com>
2025-12-02 10:28:30 -0400

    autopep

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,6 +5,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Create and return an asyncio Task that waits for a random delay."""
     return asyncio.create_task(wait_random(max_delay))
