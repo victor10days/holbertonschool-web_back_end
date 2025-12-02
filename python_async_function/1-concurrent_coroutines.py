f2ce47c826525a4e46f91ea5414571f07a01094d
Victor <victor10days@gmail.com>
2025-12-02 10:18:22 -0400

    corrections

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times and return delays in ascending order."""
     tasks = [wait_random(max_delay) for _ in range(n)]
