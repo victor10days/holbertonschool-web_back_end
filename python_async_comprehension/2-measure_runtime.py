d9d2d95ea35756a0ceba15719da495953eddb910
Victor <victor10days@gmail.com>
2025-12-02 11:27:01 -0400

    corrections

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""Run time for four parallel coroutines."""
-
+"""Run time for four parallel comprehensions."""
 
 import asyncio
 import time
 
-async_comprehension = __import__('1-concurrent_coroutines').wait_n
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime(n: int, max_delay: int) -> float:
-    """Measure the total runtime of wait_n coroutine."""
+
+async def measure_runtime() -> float:
+    """Measure runtime of executing async_comprehension 4 times."""
     start_time = time.time()
-    await async_comprehension(n, max_delay)
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
     return end_time - start_time
