70f7ce4af009af2ae5abe1e19d66c1e3eefb990c
Victor <victor10days@gmail.com>
2025-12-05 18:48:24 -0400

    correction

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -51,10 +51,8 @@ class Server:
         Returns:
             List[List]: The list of items on the requested page.
         """
-        assert isinstance(
-            page, int) and page > 0, "page must be a positive integer"
-        assert isinstance(
-            page_size, int) and page_size > 0, "page_size must be a positive integer"
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
