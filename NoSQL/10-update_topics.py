a722a6aa04b477fbdfe688df7dafe7db1455bb6d
victor10days <victor10days@gmail.com>
2025-12-11 12:39:21 -0400

    10. Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module that updates topics of a school document in MongoDB
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection: pymongo collection object
+        name (string): the school name to update
+        topics (list of strings): the list of topics approached in the school
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
