002c0eefd576ebedc0b5e5b6e2f36a4fce198a21
Victor <victor10days@gmail.com>
2025-12-14 22:11:37 -0400

    7. Airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+export default class Airport{
+    constructor(name, code){
+        this._name = name;
+        this._code = code;
+    }
+    
+    get name(){
+        return this._name;
+    }
+    
+    get code(){
+        return this._code;
+    }
+}
