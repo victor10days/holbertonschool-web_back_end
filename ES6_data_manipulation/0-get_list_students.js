7f668331bf27348179cc1ba633dc973124c013e6
victor10days <victor10days@gmail.com>
2025-12-19 18:30:31 -0400

    correction

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,7 @@
-export default function getListStudentsIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    class student {
-        constructor(id, firstName, location) {
-            this.id = id;
-            this.firstName = firstName;
-            this.location = location;
-        }
-    }
-    const listStudents = [];
-    for (const obj of students) {
-        const { id, firstName, location } = obj;
-        listStudents.push(new student(id, firstName, location));
-    }
-    return listStudents;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
