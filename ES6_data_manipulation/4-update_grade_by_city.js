1487c7778e5d9192db78250a23c0943e2841d1b3
victor10days <victor10days@gmail.com>
2025-12-21 21:57:31 -0400

    corrections

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,11 @@
-export default function updateStudentGradeByCity(students, city, newGrade) {
-    return students
-        .filter((student) => student.location === city)
-        .map((student) => ({
-            ...student,
-            grade: newGrade,
-        }));
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
 }
