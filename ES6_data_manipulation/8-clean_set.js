e914972e4b726275fd1ad5ca3decc72eb713433d
victor10days <victor10days@gmail.com>
2025-12-21 23:10:49 -0400

    correction

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string' || startString.length === 0) {
-        return '';
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  const result = [];
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
     }
-    let result = '';
-    for (const item of set) {
-        if (item.startsWith(startString)) {
-            result += item.slice(startString.length);
-        }
-    }
-    return result;
+  }
+  return result.join('-');
 }
