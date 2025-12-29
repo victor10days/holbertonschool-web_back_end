2bc9f7aefc131d2cef3e536b521b1b95b1610c4b
victor10days <victor10days@gmail.com>
2025-12-29 11:01:36 -0400

    corrections

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,7 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-    return {
-        firstName: firstName,
-        lastName: lastName,
-    };
-    Promise.resolve(signUpUser);
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
