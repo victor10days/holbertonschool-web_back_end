d69623220d0cdc844e3b76da270c5a68ad311cec
victor10days <victor10days@gmail.com>
2025-12-29 10:26:01 -0400

    corrections

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,12 @@
-export default function hangleResponseFromAPI(promise) {
-    return promise
-        .then((response) => {
-            return { status: response.status };
-        })
-        .catch(() => {
-            return { status: 500 };
-        })
-        .finally(() => {
-            console.log('Got a response from the API');
-        });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
-
