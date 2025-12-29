14d85fbb1c7638b0e9598003139cd8b134b5feab
victor10days <victor10days@gmail.com>
2025-12-29 10:55:22 -0400

    corrections

--- ES6_promise/test-2.js
+++
@@ -1,7 +0,0 @@
-import handleResponseFromAPI from './2-then.js';
-
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
-
-const promise2 = Promise.reject();
-handleResponseFromAPI(promise2);
