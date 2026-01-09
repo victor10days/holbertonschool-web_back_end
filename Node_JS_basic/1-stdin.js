4492b82a03a85cad33e0e089b7d0cabaff4998e5
victor10days <victor10days@gmail.com>
2026-01-09 15:23:36 -0400

    corrections

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  const name = data.toString();
+  process.stdout.write(`Your name is: ${name}`);
   process.exit(console.log('This important software is now closing'));
 });
