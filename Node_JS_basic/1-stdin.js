a92f7f1934b9e0c5c86f50f0036f7f6a3e008cba
victor10days <victor10days@gmail.com>
2026-01-09 15:17:27 -0400

    corrections

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
