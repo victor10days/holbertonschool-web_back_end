464ecb94416933cd57e4b7d770687a1c5b28eb63
victor10days <victor10days@gmail.com>
2026-01-09 15:15:00 -0400

    1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,4 +3,5 @@ process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
   process.exit();
+  console.log('This important software is now closing\n');
 });
