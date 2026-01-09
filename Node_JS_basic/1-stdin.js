f55bc325ea62ab47066f825cfffef96f41bf9163
victor10days <victor10days@gmail.com>
2026-01-09 15:12:52 -0400

    1. Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+console.log('Welcome to Holberton School, what is your name?\n')
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
