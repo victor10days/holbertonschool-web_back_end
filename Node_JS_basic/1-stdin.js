5fc70609354d17d68fdf366756e6a26927cea75d
victor10days <victor10days@gmail.com>
2026-01-09 15:19:19 -0400

    1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
   process.exit();
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
