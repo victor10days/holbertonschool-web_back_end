f73fc063a6872ad8c117c88cbb0e0ad40b7de32f
victor10days <victor10days@gmail.com>
2026-01-12 11:57:35 -0400

    7. Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -7,20 +7,19 @@ const app = express();
 const database = process.argv[2];
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello Holberton School!');
-    }
-);
+  res.status(200).send('Hello Holberton School!');
+});
 
 app.get('/students', async (req, res) => {
-    let responseText = 'This is the list of our students\n';
-    try {
-        const report = await countStudents(database);
-        responseText += report;
-        res.status(200).send(responseText);
-    } catch (err) {
-        responseText += err.message;
-        res.status(200).send(responseText);
-    }
+  let responseText = 'This is the list of our students\n';
+  try {
+    const report = await countStudents(database);
+    responseText += report;
+    res.status(200).send(responseText);
+  } catch (err) {
+    responseText += err.message;
+    res.status(200).send(responseText);
+  }
 });
 
 app.listen(1245, () => {});
